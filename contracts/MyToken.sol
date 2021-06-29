fab0639a25514eae169d386b606f0fdd34fa3511
Mehul Oswal <mehuloswal21@gmail.com>
2021-06-29 23:55:50 +0530

    Added ERC20 from open-zeppelin

---
+++ contracts/MyToken.sol
@@ -0,0 +1,14 @@
+// contracts/GLDToken.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("MOMO Cappucino Token", "MOMO") {
+        _mint(msg.sender, initialSupply);
+    }
+    function decimals() public view virtual override returns (uint8) {
+    return 0;
+}
+}
