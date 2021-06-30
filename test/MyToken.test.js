6c4edc4c0918acb356aa86ec8c8c254ed17233ca
Mehul Oswal <mehuloswal21@gmail.com>
2021-06-30 14:55:15 +0530

    Added More tests to token-test

--- test/MyToken.test.js
+++ test/MyToken.test.js
@@ -6,25 +6,51 @@ const chaiBN = require("chai-bn")(BN);
 chai.use(chaiBN);
 
 var chaiAsPromised = require("chai-as-promised");
-const { assert } = require("console");
-const { contracts_build_directory } = require("../truffle-config");
+
 chai.use(chaiAsPromised);
 
 const expect = chai.expect;
 
 contract("Token Test", async (accounts) => {
   //write tests here
+
+  const [deployerAccount, recipient, anotherAccount] = accounts;
+
   it("all tokens should be in my account", async () => {
     let instance = await Token.deployed();
     let totalSupply = await instance.totalSupply();
     // let balance = await instance.balanceOf(accounts[0]);
-    // assert.equal(
-    //   balance.valueOf(),
-    //   initialSupply.valueOf(),
-    //   "The balance wasnt same"
-    // );
-    expect(instance.balanceOf(accounts[0])).to.eventually.be.a.bignumber.equal(
-      totalSupply
-    );
+    // assert.equal( balance.valueOf(),initialSupply.valueOf(),"The balance wasnt same");
+    await expect(
+      instance.balanceOf(deployerAccount)
+    ).to.eventually.be.a.bignumber.equal(totalSupply);
+  });
+
+  it("is possible to send tokens between accounts", async () => {
+    const sendTokens = 1;
+    let instance = await Token.deployed();
+    let totalSupply = await instance.totalSupply();
+    await expect(
+      instance.balanceOf(deployerAccount)
+    ).to.eventually.be.a.bignumber.equal(totalSupply);
+    await expect(instance.transfer(recipient, sendTokens)).to.eventually.be
+      .fulfilled;
+    await expect(
+      instance.balanceOf(deployerAccount)
+    ).to.eventually.be.a.bignumber.equal(totalSupply.sub(new BN(sendTokens)));
+    await expect(
+      instance.balanceOf(recipient)
+    ).to.eventually.be.a.bignumber.equal(new BN(sendTokens));
+  });
+
+  it("is not possible to send more tokens than available in total", async () => {
+    let instance = await Token.deployed();
+    let balanceOfDeployer = await instance.balanceOf(deployerAccount);
+
+    await expect(instance.transfer(recipient, new BN(balanceOfDeployer + 1))).to
+      .eventually.be.rejected;
+    await expect(
+      instance.balanceOf(deployerAccount)
+    ).to.eventually.be.a.bignumber.equal(balanceOfDeployer);
   });
 });
