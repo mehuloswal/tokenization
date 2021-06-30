9193afe09a2275f463a5d77624d44a2b75f87238
Mehul Oswal <mehuloswal21@gmail.com>
2021-06-30 21:15:20 +0530

    Added Crowdable SC from OZ | Changed compiler version to ^0.6.0

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -1,14 +1,10 @@
-// contracts/GLDToken.sol
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity >=0.6.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract MyToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("MOMO Cappucino Token", "MOMO") {
+    constructor(uint256 initialSupply) ERC20("StarDucks Capu-Token", "SCT") public {
         _mint(msg.sender, initialSupply);
+    _setupDecimals(0);
     }
-    function decimals() public view virtual override returns (uint8) {
-    return 0;
-}
 }
