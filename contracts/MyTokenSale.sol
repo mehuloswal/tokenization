f9b5b027f33b90cca6956316857a1c13d2cb238d
Mehul Oswal <mehuloswal21@gmail.com>
2021-06-30 21:50:24 +0530

    Sent all the tokens to MyTokenSale SmartContract

---
+++ contracts/MyTokenSale.sol
@@ -0,0 +1,16 @@
+pragma solidity ^0.6.0;
+
+import "./Crowdsale.sol";
+
+contract MyTokenSale is Crowdsale {
+    constructor(
+        uint256 rate,    // rate in TKNbits
+        address payable wallet,
+        IERC20 token
+    )
+        Crowdsale(rate, wallet, token)
+        public
+    {
+
+    }
+}
